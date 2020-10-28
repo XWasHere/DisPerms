b3cd7e3536751ca1da29a9ed62f1561fa0907c7b
X-San <70487315+XWasHere@users.noreply.github.com>
2020-10-28 13:24:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,30 +7,3 @@ const fs = require('fs');
 
 
 
-class PermissionManager {
-  constructor(guild) {
-    this.roles = {};
-    this.friendlyRoleIndex = [];
-    this.perms = {};
-    for (const [k,v] of guild.roles.cache) {
-      this.roles[k] = new PermissionManagerRole(v);
-      this.friendlyRoleIndex[v.position] = k;
-    };
-  }
-  setPermission(role, perm, value) {
-    var r = '0';
-    if (role instanceof Discord.Role) {
-      r = role.id;
-    } else {
-      r = role;
-    }
-    this.roles[r].setPerm(perm, value);
-    if (this.perms[perm] == null) {
-      this.perms[perm] = [];
-    }
-    this.perms[perm].push(r);
-  }
-  getPermission(member, perm) {
-    
-  }
-}
