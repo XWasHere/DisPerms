b042a095ec751b18f51ec643d317cac86d992b9f
X-San <ytbaconboi95@gmail.com>
2020-10-28 17:16:44 +0000

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const Perms = require('../src/index.js');
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log("Client Ready");
+});
+
+client.on('message', (msg) => {
+
+});
+
+async function main() {
+  client.login(process.env.dtoken);
+  const testguild = await client.guilds.fetch('767871239938375690')
+  const pmgr = new PermissionManager(testguild);
+  console.log(pmgr)
+  pmgr.setPermission('767872345737658419', 'invertedcode.test', true)
+  console.log(pmgr)
+}
+
+main();
