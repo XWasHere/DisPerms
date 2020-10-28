8a020117aaf90a1fb2e09206b56a77af0dacc404
X-San <70487315+XWasHere@users.noreply.github.com>
2020-10-28 13:32:16 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ client.on('message', (msg) => {
 async function main() {
   client.login(process.env.dtoken);
   const testguild = await client.guilds.fetch('767871239938375690')
-  const pmgr = new PermissionManager(testguild);
+  const pmgr = new Perms.PermissionManager(testguild);
   console.log(pmgr)
   pmgr.setPermission('767872345737658419', 'invertedcode.test', true)
   console.log(pmgr)
